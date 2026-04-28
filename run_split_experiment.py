f86a086f197a93817e56513b750bdcdb34b95282
Jorge Mares <kokomares@gmail.com>
2026-04-27 21:16:58 -0500

    fixed requirements.txt to work with non cuda versions (apple silicon)"

---
+++ run_split_experiment.py
@@ -0,0 +1,12 @@
+from data.eurosat import get_dataloaders
+from architectures.model_factory import build_model
+from engine.train import train_model
+from engine.evaluate import evaluate_model
+from utils.config import load_config
+import datetime
+import os
+import json
+
+
+import torch
+print(torch.backend.mps.is_available())
